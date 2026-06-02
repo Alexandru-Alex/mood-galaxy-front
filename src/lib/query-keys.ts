6ac9fcd4a23937629ebb8ecc6d0bd5edf9507b55
Alexandru-Alex <alexstefan_ct@yahoo.com>
2026-06-02 18:21:38 +0300

    feat: add account query key and DTO

---
+++ src/lib/query-keys.ts
@@ -0,0 +1,9 @@
+export const ACCOUNT_QUERY_KEY = ['account'] as const;
+
+export type AccountDto = {
+  id: string;
+  email: string;
+  displayName: string;
+  notification?: boolean;
+  provider: string;
+};
